6e66d9ae2c0c39a122b1705069d8b8c5454613b2
thothdragon <thothdragons@gmail.com>
2020-01-29 10:09:38 +0100

    Add Routing Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChannelComponent } from './channel/channel.component';
 
 
+const routes: Routes = [
+  {
+    path: `login`, component: LoginComponent
+  },
+  {
+    path: `profile`, component: ProfileComponent
+  },
+  {
+    path: `channel`, component: ChannelComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
