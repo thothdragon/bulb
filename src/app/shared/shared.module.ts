25e50034be58a52cf9a995321edb1190697671bc
thothdragon <thothdragons@gmail.com>
2020-01-29 14:11:35 +0100

    Preparation Template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot()
   ],
   exports: [
-    IonicModule
+    IonicModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
