90b0a64a97c093f10b4ba6edbeee0ff4ae2e2dcc
thothdragon <thothdragons@gmail.com>
2020-02-03 17:12:44 +0100

    Upgrade Services & work on the tchat

---
+++ src/environments/json-bin.ts
@@ -0,0 +1,7 @@
+export const jsonBin = {
+    token: `$2b$10$t/8hIWddEcTetnWKjwWmq.8my.RuFoHWQ3dZYK1HTfJvcwCdm46RC`,
+    bin: {
+        message: `5e383d63593fd741857570e4`
+    },
+    path: `https://api.jsonbin.io/b/`
+};
